984ba8a19bb03379a9d54434b285a4272ec0b8aa
Filgiting <lextermenf@gmail.com>
2020-04-09 22:37:01 +0600

    26. React JS (структура проекта, ещё раз про ui - bll)

--- src/App.js
+++ src/App.js
@@ -5,11 +5,10 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from "./components/Dialogs/Dialogs";
-import News from "./components/News/News";
 import {BrowserRouter, Route} from "react-router-dom";
 
 
-const App = () => {
+const App = (props) => {
     return (
         <BrowserRouter>
             <div className='app-wrapper'>
@@ -21,7 +20,6 @@ const App = () => {
                     <Route path='/dialogs' component={Dialogs} />
                     <Route path='/profile' component={Profile} />
                 </div>
-
             </div>
         </BrowserRouter>
 
