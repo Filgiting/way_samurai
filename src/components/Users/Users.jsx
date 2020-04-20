d126169121f9d3ac81b886ce193717a3d838e8d7
Filgiting <lextermenf@gmail.com>
2020-04-20 13:12:55 +0600

    54. Жизненный цикл, life cycle methods, componentDidMount

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,9 +5,7 @@ import userPhoto from '../../assets/images/User.png';
 
 class Users extends React.Component {
 
-    constructor(props) {
-        super(props);
-
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
             this.props.setUsers(response.data.items)
         });
