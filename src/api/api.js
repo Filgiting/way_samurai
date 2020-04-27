d87de54a1ead083fda0a8b4577ca7bf23a0dfb70
Filgiting <lextermenf@gmail.com>
2020-04-27 16:19:55 +0600

    67. React JS - redux-thunk 2 в деталях

--- src/api/api.js
+++ src/api/api.js
@@ -23,6 +23,15 @@ export const usersAPI = {
 
     unfollow(userId) {
         return instance.delete(`follow/${userId}`)
-    }
+    },
 
+    getProfile(userId) {
+        return instance.get(`profile/${userId}`)
+    }
+};
+
+export const authAPI = {
+    me() {
+        return instance.get(`auth/me`)
+    }
 };
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,3 +1,5 @@
+import {usersAPI} from "../api/api";
+
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE_NEW_POST_TEXT';
 const SET_USER_PROFILE = 'SET_USER_PROFILE';
@@ -29,10 +31,10 @@ const profileReducer = (state = initialState, action) => {
             };
 
         case UPDATE_NEW_POST_TEXT:
-            return { ...state, newPostText: action.newText };
+            return {...state, newPostText: action.newText};
 
         case SET_USER_PROFILE:
-            return { ...state, profile: action.profile };
+            return {...state, profile: action.profile};
 
         default:
             return state;
@@ -42,5 +44,13 @@ const profileReducer = (state = initialState, action) => {
 export const addPostActionCreator = () => ({type: ADD_POST});
 export const setUserProfile = (profile) => ({type: SET_USER_PROFILE, profile});
 export const updateNewPostTextCreator = (text) => ({type: UPDATE_NEW_POST_TEXT, newText: text});
+
+export const getUserProfile = (userId) => (dispatch) => {
+
+    usersAPI.getProfile(userId)
+        .then(response => {
+            dispatch(setUserProfile(response.data));
+        });
+}
 
 export default profileReducer;
