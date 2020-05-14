b8d80b69417c6e0319463581a91bb37d90f72f6c
Filgiting <lextermenf@gmail.com>
2020-05-14 20:54:19 +0600

    93. paginator, постраничный вывод

--- src/components/common/Paginator/Paginator.jsx
+++ src/components/common/Paginator/Paginator.jsx
@@ -1,7 +1,7 @@
 import styles from './Paginator.module.css';
 import React from 'react';
 
-let Paginator = (props) => {
+let Paginator = ({portionSize = 10, ...props}) => {
 
     let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
     let pages = [];
@@ -9,6 +9,12 @@ let Paginator = (props) => {
     for (let i = 1; i <= pagesCount; i++) {
         pages.push(i);
     }
+
+    /*let portionCount = Math.ceil(pagesCount / portionSize);
+    let [portionNumber, setPortionNumber] = useState(1);
+    let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
+    let rightPortionPageNumber = portionNumber + portionSize;*/
+
 
     return <div>
 
