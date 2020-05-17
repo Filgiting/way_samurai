58dbb3a19bfca3af11a117bd49837fb4ea879283
Filgiting <lextermenf@gmail.com>
2020-05-17 17:40:06 +0600

    94. React.lazy и React.Suspense

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import {Route, withRouter} from 'react-router-dom';
 import UsersContainer from './components/Users/UsersContainer';
-import DialogsContainer from './components/Dialogs/DialogsContainer';
-import ProfileContainer from './components/Profile/ProfileContainer';
+/*import DialogsContainer from './components/Dialogs/DialogsContainer';
+import ProfileContainer from './components/Profile/ProfileContainer';*/
 import HeaderContainer from './components/Header/HeaderContainer';
 import LoginPage from './components/Login/Login';
 import {connect} from 'react-redux';
@@ -12,6 +12,10 @@ import {compose} from 'redux';
 import {initializeApp} from './redux/app-reducer';
 import Preloader from './components/common/Preloader/Preloader';
 import News from './components/News/News';
+import {withSuspense} from "./hoc/withSuspense";
+
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
 
 class App extends React.Component {
 
@@ -34,8 +38,8 @@ class App extends React.Component {
 
                 <div className='app-wrapper-content'>
 
-                    <Route path='/profile/:userId?' render={() => <ProfileContainer/>}/>
-                    <Route path='/dialogs' render={() => <DialogsContainer/>}/>
+                    <Route path='/profile/:userId?' render={withSuspense(ProfileContainer)} />
+                    <Route path='/dialogs' render={withSuspense(DialogsContainer)} />
                     <Route path='/users' render={() => <UsersContainer/>}/>
                     <Route path='/login' render={() => <LoginPage/>}/>
                     <Route path='/news' render={() => <News/>}/>
